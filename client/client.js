2585511c096bfdf29bfab9f40ee001b2365d4015
Momchill Gorchev <mgorchev@powa.com>
2014-08-19 23:10:01 +0100

    Masonry properly configured

--- client/client.js
+++ client/client.js
@@ -4,4 +4,17 @@
 
 if (Meteor.isClient) {
 
+    Template.app.rendered = function(){
+        var msnry = new Masonry('#container', {
+            // options
+            isFitWidth: true,
+            itemSelector: '.item',
+            gutter: 20,
+            containerStyle:{
+                position: 'relative',
+                width: '95%,',
+                margin: '50px auto'
+            }
+        });
+    }
 }
