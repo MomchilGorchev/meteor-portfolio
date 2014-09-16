1f5ba5732d9c31af98f5326c13508e571f4a5ad8
Momchill Gorchev <mgorchev@powa.com>
2014-09-17 00:16:49 +0100

    Refactoring the expanding effects

--- client/events.js
+++ client/events.js
@@ -3,32 +3,31 @@
  */
 Template.app.events({
     'click .expand': function(event, template){
+
         var _this = $(event.currentTarget),
             container = _this.closest('.item'),
             styles = {
-                position: 'fixed',
-                width: '100%',
-                height: '100%',
+//                position: 'fixed',
                 top: 0,
                 left: 0
             };
-        console.log(_this);
+
         Session.set('ExpandedItemStyles', container.attr('style'));
-        $('.item').not(container).animate({
-            opacity: 0.2
-            }, 500,
-            function(){
-                container.css(styles).addClass('itemExpanded');
-            }
-        );
+
+        $('.item').not(container).animate({opacity: 0.2}, 200,
+                function(){
+                    container.css(styles).addClass('itemExpanded');
+        });
     },
 
     'click .closeExpandedItem': function(event, template){
+
         var _this = $(event.currentTarget),
             container = _this.closest('.item'),
             originalStyles = Session.get('ExpandedItemStyles');
-        console.log(originalStyles);
+
         container.attr('style', originalStyles).removeClass('itemExpanded');
+
         $('.item').not(container).animate({
             opacity: 1
         }, 500);
